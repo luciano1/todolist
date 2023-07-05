7de5e43a2758ccb6e2d7bf5656bdba71390da8a1
luciano1 <lucianoantunesrocha@gmail.com>
2023-07-04 22:18:59 -0300

    Projeto para gerir tarefas diarias.

--- App.tsx
+++ App.tsx
@@ -1,27 +1,71 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Home from "./src/screens/Home";
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_500Medium,
-  Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
-import { Loading } from "./src/components/Loading";
-import { Routes } from "./src/routes";
-import { StatusBar } from "expo-status-bar";
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_500Medium,
-    Roboto_700Bold,
-  });
+const App = () => {
+  const [items, setItems] = useState([
+    { id: 1, name: 'Item 1', checked: false },
+    { id: 2, name: 'Item 2', checked: false },
+    { id: 3, name: 'Item 3', checked: false },
+  ]);
+
+  const handleToggleCheck = (itemId:number) => {
+    const updatedItems = items.map((item) => {
+      if (item.id === itemId) {
+        return { ...item, checked: !item.checked };
+      }
+      return item;
+    });
+    setItems(updatedItems);
+  };
 
   return (
-    <>
-      <StatusBar style="auto" />
-      {fontsLoaded ? <Routes /> : <Loading />}
-    </>
+    <View style={styles.container}>
+      {items.map((item) => (
+        <TouchableOpacity
+          key={item.id}
+          style={styles.itemContainer}
+          onPress={() => handleToggleCheck(item.id)}
+        >
+          <View style={styles.checkbox}>
+            {item.checked && <View style={styles.checkboxInner} />}
+          </View>
+          <Text style={styles.itemText}>{item.name}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: '#000',
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  checkboxInner: {
+    width: 12,
+    height: 12,
+    borderRadius: 2,
+    backgroundColor: '#000',
+  },
+  itemText: {
+    fontSize: 16,
+  },
+});
+
+export default App;
